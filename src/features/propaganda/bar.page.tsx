9c2111b5b20f07a7ba91005bddf1b319a2c01f0c
Rogerson Nazário <rrnazario@gmail.com>
2023-04-12 15:39:24 +0200

    Removing unused code

--- src/features/propaganda/bar.page.tsx
+++ src/features/propaganda/bar.page.tsx
@@ -5,50 +5,50 @@ import AppBar from '../../components/title/initialtitle.component';
 import Toolbar from '../../components/title/toolbar.component';
 
 const rightLink = {
-  fontSize: 16,
-  color: 'common.white',
-  ml: 3,
+    fontSize: 16,
+    color: 'common.white',
+    ml: 3,
 };
 
 function AppAppBar() {
-  return (
-    <div>
-      <AppBar position="fixed" sx={{ backgroundColor: 'black', color: 'white' }}>
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <Box sx={{ flex: 1 }} />
-          <Link
-            variant="h4"
-            underline="none"
-            color="inherit"
-            href="/"
-            sx={{ fontSize: 24 }}
-          >
-            {'ENCUENTRA PISO'}
-          </Link>
-          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Link
-              color="inherit"
-              variant="h6"
-              underline="none"
-              href="/login"
-              sx={rightLink}
-            >
-              {'Sign In'}
-            </Link>
-            <Link
-              variant="h6"
-              underline="none"
-              href="/register"
-              sx={{ ...rightLink }}
-            >
-              {'Sign Up'}
-            </Link>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </div>
-  );
+    return (
+        <div>
+            <AppBar position="fixed" sx={{ backgroundColor: 'black', color: 'white' }}>
+                <Toolbar sx={{ justifyContent: 'space-between' }}>
+                    <Box sx={{ flex: 1 }} />
+                    <Link
+                        variant="h4"
+                        underline="none"
+                        color="inherit"
+                        href="/"
+                        sx={{ fontSize: 24 }}
+                    >
+                        {'ENCUENTRA PISO'}
+                    </Link>
+                    <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+                        <Link
+                            color="inherit"
+                            variant="h6"
+                            underline="none"
+                            href="/login"
+                            sx={rightLink}
+                        >
+                            {'Sign In'}
+                        </Link>
+                        <Link
+                            variant="h6"
+                            underline="none"
+                            href="/register"
+                            sx={{ ...rightLink }}
+                        >
+                            {'Sign Up'}
+                        </Link>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <Toolbar />
+        </div>
+    );
 }
 
 export default AppAppBar;
--- src/features/propaganda/propaganda.page.tsx
+++ src/features/propaganda/propaganda.page.tsx
@@ -3,10 +3,6 @@ import FirstLevel from "./firstlevel.page";
 import PropagandaFooter from "./footer.page";
 
 export function Propaganda() {
-    const onLogout = async () => {
-       
-    }
-
     return <>
         <AppAppBar />
         <FirstLevel />
